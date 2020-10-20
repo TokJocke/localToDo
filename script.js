b09e7e5b258d1bce5954178cf496c1e4593e686a
Joakim <j-pf@live.se>
2020-10-20 14:32:16 +0200

    changes

--- script.js
+++ script.js
@@ -1,46 +1,68 @@
 
-    
-    
-
-//Skapa login funktion som loopar igenom localstorage och letar efter match
-function login() { 
-    const userNameToCheck = document.getElementById("username").value
-    const passwordToCheck = document.getElementById("password").value
+// Hämta och parsea localstorage      
+function getUserList() {
+    let userArray = localStorage.getItem("users")
+    //Om localstorage är tom, skapa en array
+    if(userArray == null) {
+        userArray = []
+    }
+    //Om localStorage finns, hämta och omvandla
+    else {
+        userArray = JSON.parse(userArray)
+    } 
+    console.log(userArray, "getUserFunction")
+    return userArray
 }
-//Registrera ny användare
-function registerUser() {
-    let registerUserName = document.getElementById("registerUserName")
-    let registerPassword = document.getElementById("registerPassword")
-    const registerButton = document.getElementById("registerButton")
-    
-    registerButton.addEventListener("click", function(){
-        //hämta localstorage spara i userArray
-        let userArray = localStorage.getItem("users")
-        //Om localstorage är tom, skapa en array
-        if(userArray == null) {
-            userArray = []
-        }
-        //Om localStorage finns, hämta och omvandla
-        else{
-            userArray = JSON.parse(userArray)
-        }
-        // kolla om användar namnet finns i arrayen, om det finns logga "user name taken"(funkar inte)
-        if(registerUserName.value == userArray.userName){ 
-            console.log("user name taken")
-        }
-        //Om användar namnet inte finns, skapa ny användare och pusha i userArray
-        else{
-            userArray.push({
-                userName: registerUserName.value,
-                userPassword: registerPassword.value
-            })
-            localStorage.setItem("users", JSON.stringify(userArray))
-        }
-        //Skicka min userArray till localstorage och gör till string
-        
-        console.log(userArray)
+//Spara array i localstorage
+function saveArrayToLocal(arrayToLocal) {
+    localStorage.setItem("users", JSON.stringify(arrayToLocal))
+}
 
+
+function checkUser() { // Inte klar
+    let myList = getUserList() 
+    let nameToCheck = document.getElementById("registerUserName").value
+    
+    for(i = 0; i < myList.length; i++){
+
+    }
+}
+ 
+
+
+
+
+
+
+
+//Lägg till i array och skicka till localStorage
+function addToArray() {
+    let myArray = getUserList()
+    let registerUserName = document.getElementById("registerUserName").value
+    let registerPassword = document.getElementById("registerPassword").value
+    console.log(myArray, "addToArrayFunction")
+    myArray.push({
+        name: registerUserName,
+        password: registerPassword
     })
+     saveArrayToLocal(myArray)
+}
+//Kör mina funktion onclick som lägger till ny användare  i localstorage
+function addToArrayOnClick() {
+    let registerButton = document.getElementById("registerButton")
+
+    registerButton.addEventListener("click", () => {
+        
+        
+    
+     
+            addToArray()
+        
+    })
+
 }
 
-registerUser()
+
+addToArrayOnClick()
+
+
