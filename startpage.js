76942f4379483fd8f65463f3e00529fc2fe4567b
Joakim <j-pf@live.se>
2020-10-21 16:35:55 +0200

    chaaanges

--- startpage.js
+++ startpage.js
@@ -1,3 +1,4 @@
+//Kollar om användare är inloggad eller ej
 function checkSession() {
     let mySession = sessionStorage.getItem("user")
 
@@ -11,7 +12,7 @@ function checkSession() {
     }
     return mySession
 }
-
+//Dynamisk funktion bereonde på om användare är inloggad eller inte
 function loginOrLogout() {
     let loginOrOut = document.getElementById("loginOrOut")
     
@@ -26,12 +27,13 @@ function loginOrLogout() {
 
     if(checkSession() == true){
         
-        buttonText()
         console.log("du är inloggad")
+        buttonText()
         createHtmlForLoggedIn()
         loginOrOut.addEventListener("click", () => {
             sessionStorage.clear()
             loginOrLogout()
+            createHtmlForLoggedIn()
         })
         
     }
@@ -42,19 +44,20 @@ function loginOrLogout() {
         })
     }
 }
-
+//Skapa alla element som ska finnas när användare är inloggad
 function createHtmlForLoggedIn() {
-
+    //Skapa en dynamisk titel som lägger sig i Headern
     function loggedInTitle() {
         let myHeader = document.getElementById("myHeader")
         let pageTitle = document.getElementById("pageTitle")
         let loggedInTitle = document.createElement("p")
-        myHeader.insertBefore(loggedInTitle, pageTitle);
-        
         loggedInTitle.className = "loggedInTitle"
         loggedInTitle.innerText = "inloggad som" + " " + sessionStorage.user
+        
+        myHeader.insertBefore(loggedInTitle, pageTitle);
+        return loggedInTitle
     }
-
+    //Skapa en container som wrappar alla mina toDo element
     function createLoggedInWrap() {
         let main = document.getElementById("myMain")
         toDoWrap = document.createElement("div")
@@ -63,23 +66,43 @@ function createHtmlForLoggedIn() {
       
         return toDoWrap
     }
-    function createInputName() {
-        let inputToDoName = document.createElement("input")
-        inputToDoName.className = "toDoInput"
-        return inputToDoName
+    //Skapa en titel input för todo
+    function createInputTitle() {
+        let inputToDoTitle = document.createElement("input")
+        inputToDoTitle.className = "toDoInput"
+        inputToDoTitle.id = "inputToDoName"
+        inputToDoTitle.placeholder = "Title"
+        return inputToDoTitle
     }
+    //skapa en description input för todo
     function createInputDescription() {
         let inputToDoDescription = document.createElement("input")
         inputToDoDescription.className = "toDoInput"
+        inputToDoDescription.id = "inputToDoDescription"
+        inputToDoDescription.placeholder ="description"
         return inputToDoDescription
     }
-
+    //skapa en knapp för att submita todo
+    function toDoButton() {
+        let toDoButton = document.createElement("button")
+        toDoButton.className = "toDoButton"
+        toDoButton.innerText = "submit"
+        return toDoButton
+    }
+    //Fäst eller ta bort mina element på min sida
+    if(checkSession() == true){
+        loggedInTitle()
+        createLoggedInWrap()
     
-    loggedInTitle()
-    createLoggedInWrap()
-    toDoWrap.appendChild(createInputName())
-    toDoWrap.appendChild(createInputDescription())
-    
+        toDoWrap.appendChild(createInputTitle())
+        toDoWrap.appendChild(createInputDescription())
+        toDoWrap.appendChild(toDoButton()) 
+    }
+    else {
+        
+        toDoWrap.parentNode.removeChild(toDoWrap);
+        
+    }
     
 }
 
